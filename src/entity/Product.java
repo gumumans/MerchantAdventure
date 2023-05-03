0783977a99e9c6b0269ce7f7a2673006a07bbec2
Ilia Belikov <gumuman@mail.ru>
2023-05-03 20:58:09 +0600

    implement decrease quality method in Product object and implement interface Quality in enum

--- src/entity/Product.java
+++ src/entity/Product.java
@@ -1,5 +1,8 @@
 package entity;
 
+import exceptions.SellerException;
+import qualityProducts.Qualities;
+
 import static utilities.Rnd.rnd;
 
 public class Product {
@@ -7,18 +10,27 @@ public class Product {
     private final int weigth;
     private final double purchasePrice;
     private double sellingPrice;
-    private String quality;
+    private Qualities quality;
 
     public Product() {
         String[] typesProduct = {"мясо", "сухофрукты", "зерно", "мука", "ткани", "краска"};
         this.type = typesProduct[rnd(typesProduct.length)];
         this.weigth = rnd(5) + 6;
         this.purchasePrice = rnd(14) + 8;
-        this.quality = "";
+        this.quality = Qualities.NORMAL;
+    }
+
+    public void setQuality(Qualities quality) {
+        this.quality = quality;
+        System.out.printf("Состояние продукта %s изменилось на %s%n",this.type, this.quality.getName());
+    }
+
+    public void decreaseQuality() {
+        quality.decreaseQuality(this);
     }
 
     @Override
     public String toString() {
-        return String.format("%s | Вес: %d | К-во: %s | Купили за: %.2f | Продадим за: %.2f", type, weigth, quality, purchasePrice, sellingPrice);
+        return String.format("%s | Вес: %d | К-во: %s | Купили за: %.2f | Продадим за: %.2f", type, weigth, quality.getName(), purchasePrice, sellingPrice);
     }
 }
