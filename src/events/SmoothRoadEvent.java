175e0ace70f21d10b4c7fa9ca1f0a2586bc8d974
Ilia Belikov <gumuman@mail.ru>
2023-05-03 22:16:43 +0600

    Added SmoothRoadEvent

---
+++ src/events/SmoothRoadEvent.java
@@ -0,0 +1,16 @@
+package events;
+
+import entity.Seller;
+
+public class SmoothRoadEvent extends Event{
+    public SmoothRoadEvent() {
+        super("Ровная дорога", true);
+    }
+
+    @Override
+    public void doEvent(Seller seller) {
+        System.out.println("Торговцу попалась ровная дорога и ему хорошо, скорость передвижения увеличилась на 2");
+        seller.changeSpeed(2);
+        seller.moveToCity();
+    }
+}
