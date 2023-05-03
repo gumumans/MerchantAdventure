6e5db0bf7b519511b3d28ab1a443e5651f7921b5
Ilia Belikov <gumuman@mail.ru>
2023-05-04 00:45:12 +0600

    refactor code

--- src/entity/Seller.java
+++ src/entity/Seller.java
@@ -6,23 +6,23 @@ import java.util.List;
 import static utilities.Rnd.rnd;
 
 public class Seller {
-    private int speed;
-    private double balance;
-    private final int loadCapacity;
-    private final List<Product> cart;
-    private int cartCapacity;
-    private int distanceToCity;
+        private int speed;
+        private double balance;
+        private final int loadCapacity;
+        private final List<Product> cart;
+        private int cartCapacity;
+        private int distanceToCity;
 
-    public Seller(int distanceToCity) {
-        this.speed = rnd(5) + 1;
-        this.balance = rnd(301) + 200;
-        this.loadCapacity = rnd(121) + 80;
-        this.cartCapacity = 0;
-        this.cart = new ArrayList<>();
-        this.distanceToCity = distanceToCity;
-    }
+        public Seller(int distanceToCity) {
+            this.speed = rnd(5) + 1;
+            this.balance = rnd(301) + 200d;
+            this.loadCapacity = rnd(121) + 80;
+            this.cartCapacity = 0;
+            this.cart = new ArrayList<>();
+            this.distanceToCity = distanceToCity;
+        }
 
-    public void purchase(Product product) {
+        public void purchase(Product product) {
         if (balance >= product.getPurchasePrice()) {
             if (loadCapacity >= (cartCapacity + product.getWeight())) {
                 cart.add(product);
@@ -67,9 +67,6 @@ public class Seller {
         this.speed = 0;
     }
 
-    public int getSpeed() {
-        return speed;
-    }
 
     public void takeMoney(double value) {
         this.balance = (balance - value) < 0 ? 0 : balance - value;
--- src/qualityProducts/Quality.java
+++ src/qualityProducts/Quality.java
@@ -1,7 +1,6 @@
 package qualityProducts;
 
 import entity.Product;
-import exceptions.SellerException;
 
 public interface Quality {
     void decreaseQuality(Product product);
