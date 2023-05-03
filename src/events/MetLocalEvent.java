4c8096438311918c119b09e0bb9eb19c879ce6db
Ilia Belikov <gumuman@mail.ru>
2023-05-03 22:20:18 +0600

    Added MetLocalEvent

---
+++ src/events/MetLocalEvent.java
@@ -0,0 +1,18 @@
+package events;
+
+import entity.Seller;
+
+import static utilities.Rnd.rnd;
+
+public class MetLocalEvent extends Event {
+    public MetLocalEvent() {
+        super("Встретил местного", true);
+    }
+
+    @Override
+    public void doEvent(Seller seller) {
+        int shortCutDistance = rnd(4) + 3;
+        System.out.printf("удалось срезать часть пути (в этот день проехали больше на %d лиг%n)",shortCutDistance);
+        seller.moveToCity(shortCutDistance);
+    }
+}
