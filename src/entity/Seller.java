00bf9d65203bd53fb8e7c9743ea21852b058f9fe
Ilia Belikov <gumuman@mail.ru>
2023-05-03 21:41:51 +0600

    Added a method responsible for moving to the destination city

--- src/entity/Seller.java
+++ src/entity/Seller.java
@@ -11,17 +11,37 @@ public class Seller {
     private int loadCapacity;
     private List<Product> cart;
     private int cartCapacity;
+    private int distanceToCity;
 
-    public Seller() {
+    public Seller(int distanceToCity) {
         this.speed = rnd(5) + 1;
         this.balance = rnd(301) + 200;
         this.loadCapacity = rnd(121) + 80;
         this.cartCapacity = 0;
         this.cart = new ArrayList<>();
+        this.distanceToCity = distanceToCity;
     }
 
 // TODO: 03.05.2023 добавить реализацию перемещения торговца, его покупка товаров, продажа
 
+    public void moveToCity() {
+        distanceToCity = distanceToCity - speed;
+    }
+
+
+    public  void moveToCity(int value){
+        distanceToCity = distanceToCity - value - speed;
+    }
+
+    public void сhangeSpeed(int value) {
+        final int maxSpeed = 5;
+        final int minSpeed = 1;
+        this.speed = Math.max(Math.min(this.speed + value, maxSpeed), minSpeed);
+    }
+
+    public void stop() {
+        this.speed = 0;
+    }
 
     public int getSpeed() {
         return speed;
